fc02eba523bb2c9927f43da05ca919ac074bd558
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-06-09 09:49:55 +0900

    refactor: BaseResponseEntity JSON key 변경

--- src/main/java/com/unionclass/profileservice/common/response/BaseResponseEntity.java
+++ src/main/java/com/unionclass/profileservice/common/response/BaseResponseEntity.java
@@ -2,11 +2,11 @@ package com.unionclass.profileservice.common.response;
 
 import org.springframework.http.HttpStatus;
 
-public record BaseResponseEntity<T>(HttpStatus httpStatus, int code, String message, boolean success, T result) {
+public record BaseResponseEntity<T>(HttpStatus httpStatus, boolean isSuccess, int code, String message, T result) {
 
-    public BaseResponseEntity(String message, T result) {this(HttpStatus.OK, 200, message, true, result);}
+    public BaseResponseEntity(String message, T result) {this(HttpStatus.OK, true, 200, message, result);}
 
     public BaseResponseEntity(String message) {
-        this(HttpStatus.OK, 200, message, true, null);
+        this(HttpStatus.OK, true, 200, message, null);
     }
 }
